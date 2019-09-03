d1475ba43c26bb9b3849783beb77fd63cd7a29a7
dvavs <vavradylan@gmail.com>
2019-09-03 18:05:50 -0400

    authentication implemented with single User model - need to discuss whether to break into separate Recruiter and Candidate models, and need to figure out why the err response on attempt to register an already-registered email contains a GET object

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,38 @@
-var db = require("../models");
+const db = require("../models");
+const passport = require("../config/passport");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+    res.json("/members");
+  });
+
+  app.post("/api/signup", function(req, res)  {
+    console.log(req.body);
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password
+    }).then(function()  {
+      res.redirect(307, "/api/login");
+    }).catch(function(err)  {
+      res.json(err);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  app.get("/api/user_data", function(req, res)  {
+    if (!req.user)  {
+      res.json({});
+    }
+    else  {
+      res.json({
+        email: req.user.email,
+        id: req.user.id
+      });
+    }
   });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,47 @@
-var db = require("../models");
+const path = require("path");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+const isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function (app) {
+
+  // Get request for the application URL without any specific URL params
+  app.get("/", function (req, res) {
+    // If the user is already logged in...
+    if (req.user) {
+      // Redirect them to the user homepage
+      res.redirect("/members");
+    }
+    // Otherwise, send them to the index page
+    res.render("index");
+  });
+
+  // Get request for the login page specifically
+  app.get("/login", function (req, res) {
+    // If the user is already logged in...
+    if (req.user) {
+      // Redirect them to the user homepage
+      res.redirect("/members");
+    }
+    // Otherwise, send them to the index page
+    res.render("index");
+  });
+
+  // Get request for the user registration page
+  app.get("/users/register", function(req, res) {
+    // If the user is already logged in...
+    if (req.user) {
+      // Redirect them to the user homepage
+      res.redirect("/members");
+    }
+    // Otherwise, send them to the registration page
+    res.render("register");
+  })
+
+  // Get request for the 'members' page once a user is logged in
+  app.get("/members", isAuthenticated, function (req, res) {
+    // Send them to the navbar page, pass in the membername
+    res.render("navbar", {
+      membername: req.user.email
     });
   });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-};
+}
