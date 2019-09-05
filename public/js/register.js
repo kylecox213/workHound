e7420bcffa740cb07f68442654528ad4919f259a
dvavs <vavradylan@gmail.com>
2019-09-05 03:21:02 -0400

    refined User model to contain belongsTo associations with both Recruiters and Candidates; manipulated register handlebars to provide for easier data reference from radio buttons, also added a front-end verification of password - making users enter it twice to verify; beefed up form submission functionality in register JS to package up the data for sending to the DB; added code to return recruiter/candidate ID when users send a GET to api/user_data in APIroutes; chained together Recruiter and Candidate create functions to User create functions upon API call to register new account, accomplishing a link at the same time; currently trying to overcome a strange error with account creation where it doesn't seem to matter what I put into the conditional statement on line 17 of apiRoutes JS, it either always puts new users into the Recruiter or Candidate category (but doesn't effectively parse between them)

--- public/js/register.js
+++ public/js/register.js
@@ -3,42 +3,90 @@ $(document).ready(function () {
   let signUpForm = $("form.signup");
   let emailInput = $("input#email-input");
   let passwordInput = $("input#password-input");
+  let passwordVerify = $("input#password-verify");
+  let firstNameInput = $("input#firstname");
+  let lastNameInput = $("input#lastname");
 
   // Enable tooltips on the page
   $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-  })
+  });
 
-  // When the signup button is clicked, we validate the email and password are not blank
+  // When the signup button is clicked...
   signUpForm.on("submit", function (event) {
+    // Prevent the page from reloading
     event.preventDefault();
+    // Create a reference to the password 
+    let verifiedPass;
+    // If the initial password input and the verifcation input match...
+    if (passwordInput.val().trim() === passwordVerify.val().trim()) {
+      // Set the password equal to the initial input
+      verifiedPass = passwordInput.val().trim();
+      // Otherwise it will remain falsey and fail the test below
+    }
+    // If the user doesn't have a proper password by now...
+    if (!verifiedPass) {
+      // Alert them as much
+      alert("Your passwords do not match.")
+      // Return from the function to prevent the final signup from going through
+      return;
+    }
+    // Grab the value of whichever radio button is checked under account type
+    let acctType = $("input[name='acct-type']:checked").val();
+    // Pull in the rest of the form fields' data
     let userData = {
       email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      password: verifiedPass,
+      firstName: firstNameInput.val().trim(),
+      lastName: lastNameInput.val().trim()
     };
-    console.log(JSON.stringify(userData));
-    if (!userData.email || !userData.password) {
+    // If the user selected an account type of Candidate...
+    if (acctType === "candidate") {
+      // Set boolean values to reflect that
+      userData.isCandidate = true;
+      userData.isRecruiter = false;
+    }
+    // If the user selected an account type of Recruiter...
+    if (acctType === "recruiter") {
+      // Set boolean values to reflect that
+      userData.isCandidate = false;
+      userData.isRecruiter = true;
+    }
+    // If the user doesn't have an email, or hasn't chosen an account type...
+    if (!userData.email || (!userData.isCandidate && !userData.isRecruiter)) {
+      // Don't let them proceed
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData);
+    // Clear the values from the input fields
     emailInput.val("");
     passwordInput.val("");
+    firstNameInput.val("");
+    lastNameInput.val("");
   });
 
-  // Does a post to the signup route. If succesful, we are redirected to the members page
-  // Otherwise we log any errors
-  function signUpUser(email, password) {
+
+  // Function to send the user registration data to the API
+  function signUpUser(userData) {
+    // Send a POST query to the API URL containing the data related to user form input
     $.post("/api/signup", {
-      email: email,
-      password: password
+      email: userData.email,
+      password: userData.password,
+      isCandidate: userData.isCandidate,
+      isRecruiter: userData.isRecruiter,
+      firstName: userData.firstName,
+      lastName: userData.lastName
+      // On callback, append the user data to the window so the user is logged in immediately
     }).then(function (data) {
       window.location.replace(data);
-      // If there's an error, handle it by throwing up a boostrap alert
+      // If there's an error, handle it
     }).catch(handleLoginErr);
-  }
+  };
 
+  // Function to handle errors with post-registration login
   function handleLoginErr(err) {
+    // Throw up an alert with the error message
     $("#alert .msg").text(err.responseJSON);
     $("#alert").fadeIn(500);
   }
